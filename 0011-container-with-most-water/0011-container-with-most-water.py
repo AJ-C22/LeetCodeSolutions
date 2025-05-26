32440d7f4757cb7b85f9f99a277990309a098236
Ajai C <114104270+AJ-C22@users.noreply.github.com>
2025-05-26 16:46:02 -0400

    Time: 80 ms (89.35%), Space: 20.7 MB (85.82%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,24 +4,22 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        l = len(height)
+        j = len(height) - 1
+        curr = 0
         i = 0
-        k = l - 1
-        m = 0
-        while i != k:
-            print(m)
-            print(k)
-            print(i)
-            area = (k - i) * min(height[i], height[k])
-            if area > m:
-                m = area
-            if height[i] <= height[k]:
+        while i < j:
+            wid = min(height[i], height[j])
+            length = j - i
+            area = wid * length
+            if wid == height[i]:
                 i += 1
-            elif height[i] > height[k]:
-                k -= 1
+            else:
+                j -= 1
+            if area > curr:
+                curr = area
+
+        return curr
+            
+
         
-        return m
-
-
-# 1 4 6 5 2 10 9 4 11
-# 0 1 2 3 4 5  6 7 8 
+        
